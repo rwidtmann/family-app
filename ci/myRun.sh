23aef8325f7978d39ffff9167a9bf9112226fb66
richardwidtmann <kato1rjw>
2017-10-29 13:52:53 -0500

    wip

--- ci/myRun.sh
+++ ci/myRun.sh
@@ -6,7 +6,7 @@ echo "Concourse API target ${fly_target}"
 echo "ci $(basename $DIR)"
 
 pushd $DIR
-  fly -t ${fly_target} execute -c task_hello_world.yml
-  cd ..
-  cf push family-app -p build/libs/family-app-1.0-SNAPSHOT.jar
+  echo 'I am in the script'
+  # cd ..
+  # cf push family-app -p build/libs/family-app-1.0-SNAPSHOT.jar
 popd
