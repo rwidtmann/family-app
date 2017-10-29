7989e329b22bce19d0444313074013b4254f6df8
richardwidtmann <kato1rjw>
2017-10-29 14:51:32 -0500

    wip

--- ci/myRun2.sh
+++ ci/myRun2.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-
-  echo I am in the script
-  cd ..
-  cf push family-app -p build/libs/family-app-1.0-SNAPSHOT.jar
+cd ..
+cf push family-app -p build/libs/family-app-1.0-SNAPSHOT.jar
